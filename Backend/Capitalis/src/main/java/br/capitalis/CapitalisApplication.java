dedce62ef8ba87b5fe1dcae38c65276ec45e1918
dju4mota <fernandomotajunior2@gmail.com>
2024-09-19 11:07:57 -0300

    Criando projeto Backend

---
+++ Backend/Capitalis/src/main/java/br/capitalis/CapitalisApplication.java
@@ -0,0 +1,13 @@
+package br.capitalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalisApplication.class, args);
+    }
+
+}
---
+++ Backend/Capitalis/src/test/java/br/capitalis/CapitalisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.capitalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
