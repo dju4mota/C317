7a22454e4cc7f6f088c7897d4684e94effcf180f
dju4mota <fernandomotajunior2@gmail.com>
2024-10-30 17:12:32 -0300

    descricao bug

--- Backend/Capitalis/src/main/java/br/capitalis/Pesquisa/Pesquisa.java
+++ Backend/Capitalis/src/main/java/br/capitalis/Pesquisa/Pesquisa.java
@@ -22,21 +22,22 @@ public class Pesquisa {
     private Long id;
     private String titulo;
     private Long id_criador;
-
     private String descricao;
     @OneToMany(mappedBy = "pesquisa")
     private List<Pergunta> perguntas;
 
 
-    public Pesquisa(Long id, String titulo, Long id_criador) {
+    public Pesquisa(Long id, String titulo, Long id_criador, String descricao) {
         this.id = id;
         this.titulo = titulo;
         this.id_criador = id_criador;
+        this.descricao = descricao;
     }
 
     public Pesquisa(PostPesquisa dtoPesquisa) {
         this.id_criador = dtoPesquisa.id_criador();
         this.titulo = dtoPesquisa.titulo();
+        this.descricao = dtoPesquisa.descricao();
     }
 
     public Pesquisa() {
